8b05585c5d06efe6124de2b8cd832df1e9a91e56
Alex Kahn <alex.kahn@outlook.com>
2014-11-21 20:53:31 +0000

    adds validators to User and Restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_format_of :email, :with=> /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,:on => :create
+
   def owner?
     self.role == 'owner'
   end

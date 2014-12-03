fb05763c3c3503fc8dfcbb3c1992490a2100ea71
alexkahn <alex.kahn@outlook.com>
2014-12-02 22:23:13 -0600

    modifies the associtation between restaurants and categories to use the more sensible has_many_and_belongs_to_many relation.

--- db/migrate/20141123221004_create_restaurants_categories.rb
+++ db/migrate/20141123221004_create_restaurants_categories.rb
@@ -1,6 +1,6 @@
-class CreateRestaurantsCategories < ActiveRecord::Migration
+class CreateCategoriesRestaurants < ActiveRecord::Migration
   def change
-  	create_table :restaurants_categories, id: false do |t|
+  	create_table :categories_restaurants, id: false do |t|
   		t.belongs_to :restaurant
   		t.belongs_to :category
   	end

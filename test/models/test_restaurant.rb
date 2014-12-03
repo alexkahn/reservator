e10767742286bb6c1786ea4b2732e4abcb71dd5c
alexkahn <alex.kahn@outlook.com>
2014-12-02 23:12:06 -0600

    adds some unit tests for model logic

--- test/models/test_restaurant.rb
+++ test/models/test_restaurant.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  test 'the truth' do
-    assert true
+  
+  test 'cannot create a restaurant without any parameters' do
+    restaurant = Restaurant.new
+    assert !restaurant.valid?
+  end
+
+  test 'cannot create a restaurant without a valid owner' do
+    r = Restaurant.new
+    r.name = "Test Restaurant"
+    # r.owner = owners(:tom)
+    assert !r.valid?, "Created a restaurant without an owner"
   end
 end
